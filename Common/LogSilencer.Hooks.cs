bdfe8c97ca4f48d918c9344d53967533af0c9b14
prolo <prolofresh@gmail.com>
2023-01-24 08:22:43 -0500

    Init commit

---
+++ Common/LogSilencer.Hooks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace Log_Silencer
+{
+	public partial class LogSilencer_Core
+	{
+		private static class Hooks
+		{
+			static void Init()
+			{
+				Harmony.CreateAndPatchAll(typeof(Hooks), GUID);
+
+			}
+		}
+
+	}
+}
---
+++ [HS2] Log Silencer/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Log_Silencer;
+
+[assembly: AssemblyTitle("HS2 Character Morpher")]
+[assembly: AssemblyProduct("HS2 Character Morpher")]
+[assembly: AssemblyDescription("Stop different types logs from printing in the log file")]
+[assembly: AssemblyCompany("https://github.com/Prolo1/")]
+[assembly: AssemblyVersion(LogSilencer_Core.Version)]
